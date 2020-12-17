9145f1d5bafb3d7ac45365e622667b42fa3fab41
Oscar Peña <subtitular@gmail.com>
2020-12-17 04:09:07 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div> 
+<h3>Documento</h3>
+</div>
+
+</template>
+
+<script>
+/*import axios from'axios';
+export default {
+  name: 'App',
+  data: function(){
+    return { tipo_documentos:[]
+    }
+  },
+  created: function(){
+    let self = this
+    axios.get("http://127.0.0.1:8000/tipo_documento")
+      .then((result) => {  
+        self.tipo_documentos = result.data
+      }).catch((error) => { alert(error)});
+  }
+}*/
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TipoDocumento.vue
@@ -0,0 +1,43 @@
+<template>
+<div> 
+  <label for="cars">Tipo de Documento</label>
+
+<select name="tipo_documento" id="tipo_documento">
+  <option v-for="(value,index) in tipo_documentos" v-bind:key="index">{{value.nombre}}</option>
+  
+</select>
+
+
+
+</div>
+
+</template>
+
+<script>
+import axios from'axios';
+export default {
+  name: 'App',
+  data: function(){
+    return { tipo_documentos:[]
+    }
+  },
+  created: function(){
+    let self = this
+    axios.get("http://127.0.0.1:8000/tipo_documento")
+      .then((result) => {  
+        self.tipo_documentos = result.data
+      }).catch((error) => { alert(error)});
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
