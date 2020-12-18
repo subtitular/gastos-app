015daae7694e6480dcbba84c331dab9dbb4f1543
Didier Pasuy <didierpasuy@gmail.com>
2020-12-18 02:01:23 -0500

    Formulario personas funcional
    El formulario ya trae los datos de la base de datos en memoria y los pinta en el formulario

--- src/components/Persona.vue
+++ src/components/Persona.vue
@@ -1,50 +1,60 @@
 <template>
-<div> 
-  <h3>Nombres</h3><input type="text"/><br>
-  <h3>Apellidos</h3><input type="text"/><br>
-  <h3>Fecha de Nacimiento</h3><input type="date" id="start" name="trip-start"
-       value="2005-06-30"
-       min="1900-01-01" >
-  <h3>Nacionalidad</h3><input type="text"/><br>
-  <documento></documento>
-  <button>Enviar</button>
-</div>
-
+  <div id=Persona class="persona">
+    <h3>Nombres</h3>
+    <input type="text" v-model="nombres.nombres">
+    <br />
+    <h3>Apellidos</h3>
+    <input type="text" v-model="nombres.apellidos">
+    <br />
+    <h3>Fecha de Nacimiento</h3>
+    <input
+      type="date"
+      v-model = fecha_nacimiento
+    />
+    <h3>Nacionalidad</h3>
+    <input type="text" v-model="nombres.nacionalidad">
+    <br />
+    <br />
+    <br />
+    <button>Enviar</button>
+  </div>
 </template>
 
 <script>
-import axios from'axios';
-import Documento from './Documento';
+import axios from "axios";
+import moment from "moment";
+import Documento from "./Documento";
 
 export default {
-  name: 'Persona',
-  components:{
-    Documento
+  name: "Persona",
+  components: {
+    Documento,
   },
-  data: function(){
-    return { idpersona:0
-    }
-  }
-  ,
-  created: function(){
-            console.log(idpersona)
-            this.idpersona = this.$route.params.idpersona
-        },
-  created: function(){
-    let self = this
-    console.log(idpersona)
-    axios.get("http://127.0.0.1:8000/persona/{idpersona}")
-      .then((result) => {  
-        self.tipo_documentos = result.data
-        console.log(result.data)
-      }).catch((error) => { alert(error)});
-  }
-}
+  data: function () {
+    return {
+      idpersona: 0,
+      nombres: "",
+      fecha_nacimiento: ""
+    };
+  },
+  created: function () {
+    this.idpersona = this.$route.params.idpersona;
+
+    let self = this;
+    axios .get("http://127.0.0.1:8000/persona/" + self.idpersona).then((result) => {
+        self.nombres = result.data[0];
+        self.fecha_nacimiento = moment(self.nombres.fecha_nacimiento).format('YYYY-MM-DD')
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
